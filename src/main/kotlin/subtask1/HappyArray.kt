1e4b5026b3a9520cbb8d4df8ddc76b02c6c0e4f5
GlebLogachev <GlebLogachev@users.noreply.github.com>
2021-05-30 14:27:51 +0700

    feat : add solution for task 1 (part 1)

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,21 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        val happyArray = mutableListOf<Int>()
+        if (sadArray.isNotEmpty()) {
+            happyArray.add(sadArray[0])
+            for (i in 1 until sadArray.lastIndex) {
+                if (sadArray[i] <= happyArray.last() + sadArray[i + 1])
+                    happyArray.add(sadArray[i])
+                else {
+                    for (j in happyArray.lastIndex downTo 1) {
+                        if (happyArray[j] > happyArray[j - 1] + sadArray[i + 1]) happyArray.removeAt(j)
+                    }
+                }
+            }
+            happyArray.add(sadArray.last())
+        }
+        return happyArray.toIntArray()
     }
 }
