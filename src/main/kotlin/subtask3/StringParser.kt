8701fcac41a016b9593468ae245806f1699013ec
GlebLogachev <GlebLogachev@users.noreply.github.com>
2021-05-30 14:30:23 +0700

    feat : add solution for task 1 (part 3)

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -1,9 +1,53 @@
 package subtask3
 
 class StringParser {
-
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val result = mutableListOf<String>()
+        var count: Int
+        var min = 0
+        val max = inputString.length - 1
+        val chars = listOf('(', ')', '<', '>', '[', ']')
+
+        for (i in 0..max) {
+            if (chars.contains(inputString[i])) {
+                when (inputString[i]) {
+                    '(' -> {
+                        if (i < max)
+                            min = i + 1
+                        count = 0
+                        for (j in min..max) {
+                            when (inputString[j]) {
+                                ')' -> if (count == 0) result.add(inputString.substring(min, j))
+                                else count--
+                                '(' -> count++
+                            }
+                        }
+                    }
+                    '[' -> {
+                        if (i < max) min = i + 1
+                        count = 0
+                        for (j in min..max) {
+                            when (inputString[j]) {
+                                ']' -> if (count == 0) result.add(inputString.substring(min, j))
+                                else count--
+                                '[' -> count++
+                            }
+                        }
+                    }
+                    '<' -> {
+                        if (i < max) min = i + 1
+                        count = 0
+                        for (j in min..max) {
+                            when (inputString[j]) {
+                                '>' -> if (count == 0) result.add(inputString.substring(min, j))
+                                else count--
+                                '<' -> count++
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return result.toTypedArray()
     }
 }
